eb136ca8907331bd85fa2346e73e5406fa58a998
Oliver Kim <oliver.santi.kim@gmail.com>
2021-12-03 13:49:44 -0800

    finished part 2

--- day01/sonar_sweep.py
+++ day01/sonar_sweep.py
@@ -1,14 +1,31 @@
 # Part 1
 
-with open('test', 'r') as f:
+with open('input', 'r') as f:
     lines = f.readlines()
+    data = [int(x) for x in lines]
+
+
 
 inc_count = 0
 
-for idx in range(1, len(lines)):    
-    prev = int(lines[idx - 1])
-    curr = int(lines[idx])
+for idx in range(1, len(data)):    
+    prev = data[idx - 1]
+    curr = data[idx]
     if curr >= prev:
+        inc_count += 1
+
+print(inc_count)
+
+# Part 2
+inc_count = 0
+for idx in range(3, len(data)):
+    a = data[idx - 3]
+    b = data[idx - 2]
+    c = data[idx - 1]
+    d = data[idx]
+    prev = a + b + c
+    curr = b + c + d
+    if curr > prev:
         inc_count += 1
 
 print(inc_count)
