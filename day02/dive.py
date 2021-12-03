e614270ccbad8c0496c1f42beb0ee68a525d0036
Oliver Kim <oliver.santi.kim@gmail.com>
2021-12-03 14:00:56 -0800

    finished part 2

--- day02/dive.py
+++ day02/dive.py
@@ -6,14 +6,30 @@ with open('input', 'r') as f:
 
 x_pos = 0
 y_pos = 0
+
+# part 1
+# for cmd in commands:
+#     direction = cmd[0]
+#     value = cmd[1]
+#     if direction == 'forward':
+#         x_pos += value
+#     elif direction == 'up':
+#         y_pos -= value
+#     elif direction == 'down':
+#         y_pos += value
+
+# part 2
+aim = 0
 for cmd in commands:
     direction = cmd[0]
     value = cmd[1]
     if direction == 'forward':
         x_pos += value
+        y_pos += aim * value
     elif direction == 'up':
-        y_pos -= value
+        aim -= value
     elif direction == 'down':
-        y_pos += value
+        aim += value
+
 
 print(y_pos * x_pos)
